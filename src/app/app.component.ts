9a6199ec53f3bc34527455b4066d100f08be038c
FabianAngelesCS <cruzfabian292@gmail.com>
2024-09-16 21:08:19 -0600

    constryendo nav-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavVarComponent } from './components/nav-var/nav-var.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavVarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
